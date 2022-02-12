c8ea683bb44537e4b392689dac4f78545bcc8681
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-12 12:35:52 +0000

    passanger class made

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,10 @@
+public class Passenger {
+
+    private String name;
+    private int numberOfBags;
+
+    public Passenger(String name, int numberOfBags){
+        this.name = name;
+        this.numberOfBags = numberOfBags;
+    }
+}
