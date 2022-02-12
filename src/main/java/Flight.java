01457c063b0bd3babdf67e48294405e4c9752259
Toms Druvaskalns <tomsdruvas@gmail.com>
2022-02-12 15:50:13 +0000

    made flightTest class finished

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -22,4 +22,51 @@ public class Flight {
         this.plane = plane;
 
     }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+    public ArrayList<CabinCrewMember> getCabinCrewMembers() {
+        return cabinCrewMembers;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+    public int passengerListSize(){
+        return this.passengers.size();
+    }
+
+    public void addPassengerToFlight(Passenger passenger){
+        if(this.passengerListSize() < this.plane.getPlaneType().getCapacity()){
+        this.passengers.add(passenger);
+        };
+    }
+
+    public void addCrewMemberToFlight(CabinCrewMember crewMember){
+        this.cabinCrewMembers.add(crewMember);
+    }
+
+
 }
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+    Flight flight;
+    Pilot pilot;
+    Plane plane;
+    CabinCrewMember cabinCrewMember1;
+    CabinCrewMember cabinCrewMember2;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+
+
+
+    @BeforeEach
+    void setUp() {
+        cabinCrewMember1 = new CabinCrewMember("Jack", RankType.LeadFlightAttendant);
+        cabinCrewMember2 = new CabinCrewMember("Sean", RankType.FligthAttendant);
+        passenger1 = new Passenger("Paul", 3);
+        passenger2 = new Passenger("Ronald", 4);
+        passenger3 = new Passenger("Donald", 4);
+        plane = new Plane(PlaneType.BOEING123);
+        pilot = new Pilot("JoJo", RankType.Captain, "AB123");
+        flight = new Flight("EDI", "GLA", "GLA47", "16:00", pilot, plane);
+        flight.addCrewMemberToFlight(cabinCrewMember1);
+        flight.addCrewMemberToFlight(cabinCrewMember2);
+    }
+
+    @Test
+    void getDestination() {
+        assertEquals("EDI", flight.getDestination());
+    }
+
+    @Test
+    void getDeparture() {
+        assertEquals("GLA", flight.getDeparture());
+    }
+
+    @Test
+    void getFlightNumber() {
+        assertEquals("GLA47", flight.getFlightNumber());
+    }
+
+    @Test
+    void getDepartureTime() {
+        assertEquals("16:00", flight.getDepartureTime());
+    }
+
+    @Test
+    void getPilot() {
+        assertEquals(pilot, flight.getPilot());
+    }
+
+    @Test
+    void getCabinCrewMembers() {
+        assertEquals(2,flight.getCabinCrewMembers().size());
+
+    }
+
+    @Test
+    void getPassengers() {
+        assertEquals(2,flight.passengerListSize());
+    }
+
+    @Test
+    void getPlane() {
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    void passengerListSize() {
+        assertEquals(2, flight.passengerListSize());
+
+    }
+
+    @Test
+    void addPassengerToFlight() {
+        flight.addPassengerToFlight(passenger3);
+        assertEquals(3, flight.passengerListSize());
+
+    }
+}
